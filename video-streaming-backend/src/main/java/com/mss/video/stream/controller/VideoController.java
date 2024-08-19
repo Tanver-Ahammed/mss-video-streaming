8e3a74b26bf96df9d16383fd72906787640d0fcc
Md. Tanver Ahammed <67496397+Tanver-Ahammed@users.noreply.github.com>
2024-08-19 12:24:41 +0600

    simple video streaming back-end add

--- video-streaming-backend/src/main/java/com/mss/video/stream/controller/VideoController.java
+++ video-streaming-backend/src/main/java/com/mss/video/stream/controller/VideoController.java
@@ -4,12 +4,18 @@ import com.mss.video.stream.entities.Video;
 import com.mss.video.stream.payload.CustomMessage;
 import com.mss.video.stream.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.HandlerMapping;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,6 +25,9 @@ public class VideoController {
 
     @Autowired
     private VideoService videoService;
+    @Qualifier("resourceHandlerMapping")
+    @Autowired
+    private HandlerMapping resourceHandlerMapping;
 
     // video uploader
     @PostMapping
@@ -31,7 +40,7 @@ public class VideoController {
         video.setTitle(title);
         video.setDescription(description);
         video.setVideoId(UUID.randomUUID().toString());
-        Video savedVideo = videoService.save(video, file);
+        Video savedVideo = videoService.saveVideo(video, file);
 
         if (savedVideo != null) {
             return ResponseEntity.status(HttpStatus.OK).body(video);
@@ -42,17 +51,32 @@ public class VideoController {
         }
     }
 
+    // get all videos
+    @GetMapping
+    public List<Video> findAllVideos() {
+        return videoService.findAllVideos();
+    }
+
     // stream video
     @GetMapping("/stream/{videoId}")
     public ResponseEntity<Resource> stream(
             @PathVariable("videoId") String videoId
     ) {
 
-        Video video = this.videoService.findById(videoId);
+        Video video = this.videoService.findVideoById(videoId);
         String contentType = video.getContentType();
         String filePath = video.getFilePath();
+        Resource resource = new FileSystemResource(filePath);
 
-        return null;
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(resource);
     }
 
 }
