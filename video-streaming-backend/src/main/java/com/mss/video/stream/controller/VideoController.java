e76451412cf7a3799e22a96b737c02b87652e9c1
dsi <tanvermbstuit16@gmail.com>
2024-08-20 13:55:59 +0600

    chunk by chunk video-streaming add

--- video-streaming-backend/src/main/java/com/mss/video/stream/controller/VideoController.java
+++ video-streaming-backend/src/main/java/com/mss/video/stream/controller/VideoController.java
@@ -5,18 +5,16 @@ import com.mss.video.stream.payload.AppConstants;
 import com.mss.video.stream.payload.CustomMessage;
 import com.mss.video.stream.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.io.*;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.HandlerMapping;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -32,9 +30,6 @@ public class VideoController {
 
     @Autowired
     private VideoService videoService;
-    @Qualifier("resourceHandlerMapping")
-    @Autowired
-    private HandlerMapping resourceHandlerMapping;
 
     // video uploader
     @PostMapping
@@ -87,70 +82,85 @@ public class VideoController {
     }
 
     // stream video in chunks
+
+
+    // stream video in chunks
     @GetMapping("/stream/range/{videoId}")
-    public ResponseEntity<Resource> streamVideoRange(
-            @PathVariable("videoId") String videoId,
-            @RequestHeader(value = "Range", required = false) String range
-    ) {
+    public ResponseEntity<Resource> streamVideoRange(@PathVariable String videoId, @RequestHeader(value = "Range", required = false) String range) {
         System.out.println(range);
-        Video video = this.videoService.findVideoById(videoId);
+
+        Video video = videoService.findVideoById(videoId);
         Path path = Paths.get(video.getFilePath());
 
         Resource resource = new FileSystemResource(path);
+
         String contentType = video.getContentType();
 
         if (contentType == null) {
             contentType = "application/octet-stream";
+
         }
 
+        //file length
         long fileLength = path.toFile().length();
 
+
+        // range header null
         if (range == null) {
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .contentType(MediaType.parseMediaType(contentType))
-                    .body(resource);
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
         }
+
+        //calculating start and end range
 
         long rangeStart;
+
         long rangeEnd;
+
         String[] ranges = range.replace("bytes=", "").split("-");
+        rangeStart = Long.parseLong(ranges[0]);
 
-        try {
-            rangeStart = Long.parseLong(ranges[0]);
-            rangeEnd = (ranges.length > 1 && !ranges[1].isEmpty()) ? Long.parseLong(ranges[1]) : fileLength - 1;
-        } catch (NumberFormatException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
+        rangeEnd = rangeStart + AppConstants.CHUNK_SIZE - 1;
 
-        if (rangeEnd > fileLength - 1) {
+        if (rangeEnd >= fileLength) {
             rangeEnd = fileLength - 1;
         }
 
-        long contentLength = rangeEnd - rangeStart + 1;
 
+        System.out.println("range start : " + rangeStart);
+        System.out.println("range end : " + rangeEnd);
         InputStream inputStream;
+
         try {
+
             inputStream = Files.newInputStream(path);
             inputStream.skip(rangeStart);
-        } catch (IOException e) {
+            long contentLength = rangeEnd - rangeStart + 1;
+
+
+            byte[] data = new byte[(int) contentLength];
+            int read = inputStream.read(data, 0, data.length);
+            System.out.println("read(number of bytes) : " + read);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + fileLength);
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+            headers.add("X-Content-Type-Options", "nosniff");
+            headers.setContentLength(contentLength);
+
+            return ResponseEntity
+                    .status(HttpStatus.PARTIAL_CONTENT)
+                    .headers(headers)
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .body(new ByteArrayResource(data));
+
+
+        } catch (IOException ex) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + fileLength);
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Pragma", "no-cache");
-        headers.add("Expires", "0");
-        headers.add("X-Content-Type-Options", "nosniff");
 
-        headers.setContentLength(contentLength);
-
-        return ResponseEntity
-                .status(HttpStatus.PARTIAL_CONTENT)
-                .headers(headers)
-                .contentType(MediaType.parseMediaType(contentType))
-                .body(new InputStreamResource(inputStream));
     }
 
 
